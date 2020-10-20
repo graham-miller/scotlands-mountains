673f9abcccfa70a397bdcf6aa8067b5e7ebb3fdf
Graham Miller <mail@grahammiller.net>
2020-10-20 19:05:54 +0100

    Default classification

--- client/src/app/components/classifications/classifications.component.ts
+++ client/src/app/components/classifications/classifications.component.ts
@@ -1,10 +1,8 @@
 import { Component,  OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { MatSelectChange } from '@angular/material/select';
 import { ActivatedRoute } from '@angular/router';
 
 import { Classification } from 'src/app/models/Classification';
-import { Mountain } from 'src/app/models/Mountain';
 import { ClassificationsService } from 'src/app/services/classifications.service';
 
 @Component({
@@ -33,9 +31,11 @@ export class ClassificationsComponent implements OnInit {
   getClassifications(): void {
     this.classificationService
       .getClassifications()
-      .subscribe(c => {
-        this.classifications = c;
-        this.loading = false;
+      .subscribe(classifications => {
+        this.classifications = classifications;
+        if (!this.selected.id) {
+          this.displayClassification();
+        }
       });
   }
 
@@ -44,16 +44,16 @@ export class ClassificationsComponent implements OnInit {
       this.loading = true;
       this.classificationService
         .getClassification(this.selected.id)
-        .subscribe(c => {
-          this.selected = c;
+        .subscribe(classification => {
+          this.selected = classification;
           this.loading = false;
         });
     }
   }
 
-  onClassificationChange(event: MatSelectChange): void {
-    this.selected = { id: this.selected.id } as Classification;
-    const command = [(this.route.snapshot.paramMap.get('id') ? '../' : '') + this.selected.id];
+  displayClassification(): void {
+    const id = this.selected.id || this.classifications[0].id;
+    const command = [(this.route.snapshot.paramMap.get('id') ? '../' : '') + id];
     this.router.navigate(command, { relativeTo: this.route });
   }
 }
