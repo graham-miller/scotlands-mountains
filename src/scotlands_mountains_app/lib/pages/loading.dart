40070cfccfd2ad9d3f2acc597072f823255b3a25
Graham Miller <graham-miller@users.noreply.github.com>
2023-06-25 08:48:25 +0100

    WIP

--- src/scotlands_mountains_app/lib/pages/loading.dart
+++ src/scotlands_mountains_app/lib/pages/loading.dart
@@ -2,31 +2,58 @@ import 'package:flutter/material.dart';
 import '../repositories/data.dart';
 import '../widgets/sm_app_bar.dart';
 
-class Loading extends StatelessWidget {
+class Loading extends StatefulWidget {
+  final Function initializationCompleteCallback;
+
   Loading({Key? key, required this.initializationCompleteCallback})
       : super(key: key) {
     copyDbToAppData();
   }
 
-  final Function initializationCompleteCallback;
+  void copyDbToAppData() async {
+    await Data().initialize();
+    initializationCompleteCallback();
+  }
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> with TickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    )..addListener(() {
+        setState(() {});
+      });
+    _controller.repeat(reverse: true);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        appBar: SmAppBar(),
+    return Scaffold(
+        appBar: const SmAppBar(),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text('Loading'),
+              CircularProgressIndicator(
+                value: _controller.value,
+                semanticsLabel: 'Circular progress indicator',
+              ),
             ],
           ),
         ));
-  }
-
-  void copyDbToAppData() async {
-    await Data().initialize();
-
-    initializationCompleteCallback();
   }
 }
