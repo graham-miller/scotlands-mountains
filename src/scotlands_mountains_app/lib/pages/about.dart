fe0048aa6d6f2cb150280823420a3d2f5c03094a
Graham Miller <graham-miller@users.noreply.github.com>
2023-06-26 19:09:40 +0100

    WIP

--- src/scotlands_mountains_app/lib/pages/about.dart
+++ src/scotlands_mountains_app/lib/pages/about.dart
@@ -14,93 +14,76 @@ class About extends StatelessWidget {
       mainAxisSize: MainAxisSize.max,
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Row(
-            children: [
-              Expanded(
-                child: Card(
-                  child: Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: Text('Developed by Graham Miller'),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            children: [
-              Expanded(
-                child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      children: [
-                        RichText(
-                            selectionColor: Colors.black,
-                            text: TextSpan(
-                                text: 'Mountain data taken from ',
-                                style: TextStyle(
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .onSecondary),
-                                children: [
-                                  TextSpan(
-                                    text:
-                                        'The Database of British and Irish Hills v17.5',
-                                    style: TextStyle(
-                                      color:
-                                          Theme.of(context).colorScheme.primary,
-                                    ),
-                                    recognizer: TapGestureRecognizer()
-                                      ..onTap = () async {
-                                        launchUrl(
-                                            Uri.parse(
-                                                'http://hills-database.co.uk/'),
-                                            mode:
-                                                LaunchMode.externalApplication);
-                                      },
-                                  ),
-                                  const TextSpan(text: ', licensed under '),
-                                  TextSpan(
-                                    text:
-                                        'Creative Commons Attribution 4.0 International Licence',
-                                    style: TextStyle(
-                                      color:
-                                          Theme.of(context).colorScheme.primary,
-                                    ),
-                                    recognizer: TapGestureRecognizer()
-                                      ..onTap = () async {
-                                        launchUrl(
-                                            Uri.parse(
-                                                'https://creativecommons.org/licenses/by/4.0/'),
-                                            mode:
-                                                LaunchMode.externalApplication);
-                                      },
-                                  ),
-                                  const TextSpan(text: '.'),
-                                ])),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(0, 8.0, 0, 0),
-                              child: Image.asset('assets/cc-by.png'),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+        _buildAboutCard(context, const Text('Developed by Graham Miller')),
+        _buildAboutCard(context, _buildDobihCardContent(context))
       ],
     ));
+  }
+
+  Widget _buildAboutCard(BuildContext context, Widget child) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Expanded(
+              child: Card(
+                  child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: child,
+          )))
+        ],
+      ),
+    );
+  }
+
+  Column _buildDobihCardContent(BuildContext context) {
+    return Column(
+      children: [
+        RichText(
+            selectionColor: Colors.black,
+            text: TextSpan(
+                text: 'Mountain data taken from ',
+                style:
+                    TextStyle(color: Theme.of(context).colorScheme.onSecondary),
+                children: [
+                  TextSpan(
+                    text: 'The Database of British and Irish Hills v17.5',
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () async {
+                        launchUrl(Uri.parse('http://hills-database.co.uk/'),
+                            mode: LaunchMode.externalApplication);
+                      },
+                  ),
+                  const TextSpan(text: ', licensed under '),
+                  TextSpan(
+                    text:
+                        'Creative Commons Attribution 4.0 International Licence',
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () async {
+                        launchUrl(
+                            Uri.parse(
+                                'https://creativecommons.org/licenses/by/4.0/'),
+                            mode: LaunchMode.externalApplication);
+                      },
+                  ),
+                  const TextSpan(text: '.'),
+                ])),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 8.0, 0, 0),
+              child: Image.asset('assets/cc-by.png'),
+            ),
+          ],
+        )
+      ],
+    );
   }
 }
