77971d5640d9456612a48b58dbff430958d08ec9
Graham Miller <graham-miller@users.noreply.github.com>
2023-06-29 13:15:30 +0100

    WIP

--- src/scotlands_mountains_app/lib/widgets/map/mountains_map.dart
+++ src/scotlands_mountains_app/lib/widgets/map/mountains_map.dart
@@ -31,30 +31,32 @@ class _MountainsMapState extends State<MountainsMap> {
     Layer.satelliteStreets: MapboxTileLayer(styleId: 'satellite-streets-v12'),
   };
   final _fitBoundOptions = const FitBoundsOptions(
-    padding: EdgeInsets.all(8.0),
+    padding: EdgeInsets.fromLTRB(8, 8, 8, 70),
     forceIntegerZoomLevel: true,
   );
   final _maxBounds = LatLngBounds(LatLng(54, -9), LatLng(61, 0));
-
+  final _defaultCenter = LatLng(56.816922, -4.18265);
+  final _defaultZoom = 6.0;
   final _mapController = MapController();
 
   Layer _selectedLayer = Layer.outdoors;
   StreamSubscription<MapEvent>? _subscription;
   MapOptions? _mapOptions;
-  LatLngBounds? _fitBounds;
+  CenterZoom? _centerZoom;
 
   @override
   void initState() {
     super.initState();
-    _calculateFitBounds();
     _mapOptions = MapOptions(
       maxBounds: _maxBounds,
-      bounds: _fitBounds,
-      boundsOptions: _fitBoundOptions,
-      minZoom: 6,
+      center: _defaultCenter,
+      zoom: _defaultZoom,
+      minZoom: 5,
       maxZoom: 18,
       onMapReady: () {
         setState(() {
+          _calculateCenterZoom();
+          _mapController.move(_centerZoom!.center, _centerZoom!.zoom);
           _subscription = _mapController.mapEventStream.listen((event) {
             if (event is MapEventRotate) {
               setState(() {});
@@ -68,10 +70,8 @@ class _MountainsMapState extends State<MountainsMap> {
   @override
   void didUpdateWidget(MountainsMap oldWidget) {
     super.didUpdateWidget(oldWidget);
-    _calculateFitBounds();
-    final centerZoom = _mapController.centerZoomFitBounds(_fitBounds!,
-        options: _fitBoundOptions);
-    _mapController.move(centerZoom.center, centerZoom.zoom);
+    _calculateCenterZoom();
+    _mapController.move(_centerZoom!.center, _centerZoom!.zoom);
   }
 
   @override
@@ -97,8 +97,7 @@ class _MountainsMapState extends State<MountainsMap> {
         MapControls(
           onReset: () {
             _mapController.rotate(0);
-            _mapController.fitBounds(_fitBounds!,
-                options: const FitBoundsOptions(forceIntegerZoomLevel: true));
+            _mapController.move(_centerZoom!.center, _centerZoom!.zoom);
           },
           onSelectOutdoors: () {
             setState(() {
@@ -127,24 +126,29 @@ class _MountainsMapState extends State<MountainsMap> {
     );
   }
 
-  void _calculateFitBounds() {
+  void _calculateCenterZoom() {
     if (widget.mountains.isEmpty) {
-      _fitBounds = _maxBounds;
-    } else if (widget.mountains.length == 1) {
-      final mountain = widget.mountains.single;
-      _fitBounds = LatLngBounds(
-          LatLng(mountain.latitude - 0.01, mountain.longitude - 0.01),
-          LatLng(mountain.latitude + 0.01, mountain.longitude + 0.01));
+      _centerZoom = CenterZoom(center: _defaultCenter, zoom: _defaultZoom);
     } else {
-      _fitBounds = LatLngBounds(
-          LatLng(
-            widget.mountains.map((m) => m.latitude).reduce(min),
-            widget.mountains.map((m) => m.longitude).reduce(min),
-          ),
-          LatLng(
-            widget.mountains.map((m) => m.latitude).reduce(max),
-            widget.mountains.map((m) => m.longitude).reduce(max),
-          ));
+      LatLngBounds? fitBounds;
+      if (widget.mountains.length == 1) {
+        final mountain = widget.mountains.single;
+        fitBounds = LatLngBounds(
+            LatLng(mountain.latitude - 0.01, mountain.longitude - 0.01),
+            LatLng(mountain.latitude + 0.01, mountain.longitude + 0.01));
+      } else {
+        fitBounds = LatLngBounds(
+            LatLng(
+              widget.mountains.map((m) => m.latitude).reduce(min),
+              widget.mountains.map((m) => m.longitude).reduce(min),
+            ),
+            LatLng(
+              widget.mountains.map((m) => m.latitude).reduce(max),
+              widget.mountains.map((m) => m.longitude).reduce(max),
+            ));
+      }
+      _centerZoom = _mapController.centerZoomFitBounds(fitBounds,
+          options: _fitBoundOptions);
     }
   }
 }
