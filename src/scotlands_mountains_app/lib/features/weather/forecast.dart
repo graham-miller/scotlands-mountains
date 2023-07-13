baa78c4352a2e00bc28a8db415a69aa93cb6595a
Graham Miller <graham-miller@users.noreply.github.com>
2023-07-13 09:22:15 +0100

    WIP

--- src/scotlands_mountains_app/lib/features/weather/forecast.dart
+++ src/scotlands_mountains_app/lib/features/weather/forecast.dart
@@ -3,21 +3,83 @@ class Forecast {
   final String issue;
   final DateTime issued;
   final String type;
-  //final ParamUnits paramUnits;
+  final Units units;
   //final Evening evening;
-  //final List<Day> days;
+  final List<Day> days;
 
   const Forecast._(
       {required this.location,
       required this.issue,
       required this.issued,
-      required this.type});
+      required this.type,
+      required this.units,
+      required this.days});
 
   factory Forecast.fromJson(dynamic json) {
     return Forecast._(
         location: json['Location'],
         issue: json['Issue'],
         issued: DateTime.parse(json['Issued'].toString()),
-        type: json['Type']);
+        type: json['Type'],
+        units: Units.fromJson(json['ParamUnits']),
+        days: List<Day>.from(
+            json['Days']['Day'].map((j) => Day.fromJson(j)).toList()));
+  }
+}
+
+class Units {
+  final String windSpeed;
+  final String maxGust;
+  final String temperature;
+  final String feelsLike;
+
+  const Units._(
+      {required this.windSpeed,
+      required this.maxGust,
+      required this.temperature,
+      required this.feelsLike});
+
+  factory Units.fromJson(dynamic json) {
+    return Units._(
+        windSpeed: json['WindSpeed'],
+        maxGust: json['MaxGust'],
+        temperature: json['Temperature'],
+        feelsLike: json['FeelsLike']);
+  }
+}
+
+class Day {
+  final DateTime validity;
+  final String? headline;
+  final String? confidence;
+  final String? view;
+  final String? cloudFreeHillTop;
+  final String? weather;
+  final String? visibility;
+  final String? summary;
+  // final Temperature? temperature;
+  // final List<Hazard>? Hazards;
+  // final List<Period>? Periods;
+
+  const Day._(
+      {required this.validity,
+      required this.headline,
+      required this.confidence,
+      required this.view,
+      required this.cloudFreeHillTop,
+      required this.weather,
+      required this.visibility,
+      required this.summary});
+
+  factory Day.fromJson(dynamic json) {
+    return Day._(
+        validity: DateTime.parse(json['Validity'].toString()),
+        headline: json['Headline'],
+        confidence: json['Confidence'],
+        view: json['View'],
+        cloudFreeHillTop: json['CloudFreeHillTop'],
+        weather: json['Weather'],
+        visibility: json['Visibility'],
+        summary: json['Summary']);
   }
 }
